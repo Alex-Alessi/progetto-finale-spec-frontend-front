23b52b26c6cbe22dca5c55fd6f0a3532022ebbbb
Alex-Alessi <alex.alessi246@gmail.com>
2025-07-01 21:31:24 +0200

    update: favoriteDevices page layout

--- frontend/src/FavoritesDevices.jsx
+++ frontend/src/FavoritesDevices.jsx
@@ -5,6 +5,11 @@ import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
+import Form from "react-bootstrap/Form";
+import { faAndroid } from "@fortawesome/free-brands-svg-icons";
+import { faApple } from "@fortawesome/free-brands-svg-icons";
+import { faWindows } from "@fortawesome/free-brands-svg-icons";
+import { SiGarmin } from "react-icons/si";
 
 export default function FavoritesDevices() {
   const [devices, setDevices] = useState([]);
@@ -12,6 +17,8 @@ export default function FavoritesDevices() {
     const saved = localStorage.getItem("favorites");
     return saved ? JSON.parse(saved) : [];
   });
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [selectFormat, setSelectFormat] = useState("griglia");
 
   useEffect(() => {
     fetch(`http://localhost:3001/devices`)
@@ -24,10 +31,165 @@ export default function FavoritesDevices() {
     localStorage.setItem("favorites", JSON.stringify(favoriteList)); //salva i preferiti e li trasforma in stringa
   }, [favoriteList]);
 
+  function foundCategory(category) {
+    if (category === "smartphone") return "badge text-bg-info";
+    else if (category === "laptop") return "badge text-bg-warning";
+    else if (category === "tablet") return "badge text-black text-bg-success";
+    else return "badge text-black text-bg-primary";
+  }
+
+  function iconOs(os) {
+    if (os.toLowerCase().includes("android")) {
+      return (
+        <FontAwesomeIcon
+          icon={faAndroid}
+          style={{ color: "#78c258" }}
+          className="me-1"
+        />
+      );
+    } else if (
+      os.toLowerCase().includes("ios") ||
+      os.toLowerCase().includes("macos") ||
+      os.toLowerCase().includes("watchos") ||
+      os.toLowerCase().includes("ipados")
+    ) {
+      return (
+        <FontAwesomeIcon
+          icon={faApple}
+          style={{ color: "#000000" }}
+          className="me-1"
+        />
+      );
+    } else if (os.toLowerCase().includes("windows")) {
+      return (
+        <FontAwesomeIcon
+          icon={faWindows}
+          style={{ color: "#005eff" }}
+          className="me-1"
+        />
+      );
+    } else if (os.toLowerCase().includes("garmin")) {
+      return <SiGarmin className="me-1" />;
+    }
+  }
+
   return (
     <div>
-      <h1>Lista preferiti</h1>
-      {favoriteList.length == 0 ? (
+      <div className="d-flex mt-3">
+        <h1 className="w-50 mx-auto">Lista preferiti</h1>
+        <Form.Select
+          aria-label="Categoria"
+          className={`custom-select-transition ${
+            isExpanded ? "expanded" : ""
+          } me-2`}
+          onFocus={() => setIsExpanded(true)}
+          onBlur={() => setIsExpanded(false)}
+          defaultValue=""
+          onChange={(e) => {
+            setSelectFormat(e.target.value);
+          }}
+        >
+          <option value="griglia">Griglia</option>
+          <option value="lista">Lista</option>
+        </Form.Select>
+      </div>
+      {selectFormat === "griglia" ? (
+        <div className="container">
+          <div className="row">
+            {devices
+              .filter((f) => favoriteList.includes(f.id))
+              .map((d) => {
+                const imgSrc =
+                  d.media && Array.isArray(d.media) && d.media.length > 1
+                    ? `/${d.media[1]}`
+                    : null;
+                return (
+                  <div key={d.id} className="col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div className="card h-100 shadow-sm">
+                      <div className="card-body d-flex flex-column justify-content-between">
+                        <div>
+                          <p className="card-text position-absolute top-0 start-0 m-2">
+                            <span className={foundCategory(d.category)}>
+                              {d.category}
+                            </span>
+                          </p>
+                          <Button
+                            variant="outline-secondary"
+                            onClick={() => {
+                              setFavoriteList((prev) =>
+                                prev.includes(d.id)
+                                  ? prev.filter((f) => f !== d.id)
+                                  : [...prev, d.id]
+                              );
+                            }}
+                            className="btn-sm position-absolute top-0 end-0 m-2"
+                          >
+                            <FontAwesomeIcon
+                              icon={
+                                favoriteList.includes(d.id)
+                                  ? solidHeart
+                                  : regularHeart
+                              }
+                              style={{ color: "#ff0000" }}
+                            />
+                          </Button>
+                          <h5
+                            className="card-title text-center"
+                            style={{ fontSize: "18px" }}
+                          >
+                            <Link
+                              to={`/devices/${d.id}`}
+                              className="custom-link d-flex align-items-start justify-content-center"
+                              style={{ whiteSpace: "nowrap" }}
+                            >
+                              {d.storage
+                                ? `${d.title} ${d.storage}GB`
+                                : d.title}
+                            </Link>
+                          </h5>
+
+                          {imgSrc && (
+                            <img
+                              src={imgSrc}
+                              alt={d.title}
+                              className="d-block mx-auto img-fluid mb-3"
+                              style={{
+                                width: "200px",
+                                height: "150px",
+                                objectFit: "cover",
+                                marginTop: "10px",
+                              }}
+                            />
+                          )}
+                          <p>
+                            {iconOs(d.os)}
+                            {d.os}
+                          </p>
+                          <b>€ {d.price.toFixed(2).replace(".", ",")}</b>
+                          <div
+                            style={{
+                              whiteSpace: "nowrap",
+                              fontSize: "12px",
+                              borderRadius: "10px",
+                              width: "200px",
+                            }}
+                            className="bg-secondary-subtle text-secondary-emphasis mt-2"
+                          >
+                            oppure{" "}
+                            <strong>
+                              € {(d.price / 3).toFixed(2).replace(".", ",")}
+                            </strong>{" "}
+                            al mese in 3 rate
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      ) : favoriteList.length == 0 ? (
         <h2>Non ci sono elementi nei preferiti</h2>
       ) : (
         <Table bordered hover>
@@ -46,7 +208,9 @@ export default function FavoritesDevices() {
                 <tr key={d.id}>
                   <td>{d.id}</td>
                   <td>
-                    <Link to={`/devices/${d.id}`}>{d.title}</Link>
+                    <Link to={`/devices/${d.id}`} className="text-dark">
+                      {d.title}
+                    </Link>
                   </td>
                   <td>{d.category}</td>
                   <td>{new Date(d.createdAt).toLocaleDateString()}</td>
